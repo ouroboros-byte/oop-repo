caf52e4792e26ab4be225c1cc53bfc4710476b49
spragg <96534873+ouroboros-byte@users.noreply.github.com>
2022-08-30 17:38:20 +0100

    Progress
    class v static, inheritance, getters and setters

--- main.py
+++ main.py
@@ -1,39 +1,8 @@
-import csv
+from item import Item
+from phone import Phone
 
-class Item:
-    pay_rate = 0.8 # cost multiplyer with 20% discount
-    all = []
-    
-    def __init__(self, name: str, price: float, quantity = 0):
-        # Check validity of assigned arguments
-        assert price >= 0, "Price input must be more than 0."
-        assert quantity >= 0, "Quantity input must be more than 0."
+item1 = Item("demoItem", 750)
 
-        # Assign properties to self 
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-        print(f"Instance created: {name}")
+item1.name = "test"
 
-        # Actions to execute
-        Item.all.append(self)
-
-    def liquid_asset_value(self):
-        return self.price * self.quantity
-
-    def apply_discount(self):
-        self.price = self.price * self.pay_rate
-
-    @classmethod
-    def instantiate_from_csv(cls):
-        with open('items.csv', 'r') as f:
-            reader = csv.DictReader(f)
-            items = list(reader)
-
-        for item in items:
-            print(item)
-
-    def __repr__(self):
-        return f"Item('{self.name}', {self.price}, {self.quantity})"
-
-Item.instantiate_from_csv()
+print(item1.name)
