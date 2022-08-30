e06d2b33ba5eae3108377e7c4810ee4ac8e92e75
spragg <96534873+ouroboros-byte@users.noreply.github.com>
2022-08-30 10:52:58 +0100

    instantiating CSV
    ...

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
+import csv
+
 class Item:
-    def __init__(self, name: str, price: float, quantity: int = 0):
-        
+    pay_rate = 0.8 # cost multiplyer with 20% discount
+    all = []
+    
+    def __init__(self, name: str, price: float, quantity = 0):
         # Check validity of assigned arguments
         assert price >= 0, "Price input must be more than 0."
         assert quantity >= 0, "Quantity input must be more than 0."
 
-        # Assign properties to self
+        # Assign properties to self 
         self.name = name
         self.price = price
         self.quantity = quantity
         print(f"Instance created: {name}")
 
+        # Actions to execute
+        Item.all.append(self)
+
     def liquid_asset_value(self):
         return self.price * self.quantity
 
-item1 = Item("Phone", 500, 5)
-item2 = Item("Laptop", 1000, 3)
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
 
-print(item1.liquid_asset_value())
-print(item2.liquid_asset_value())
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            print(item)
+
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
+
+Item.instantiate_from_csv()
